fd75b71d3e0ac4378f58fc20c2d217820bef309b
Gabriel Pereira <arthur738pereira@gmail.com>
2026-02-01 16:51:26 -0300

    ajuste nas chaves

--- services/supabase.ts
+++ services/supabase.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Essas chaves são exemplos. O usuário deve substituir pelas chaves do seu projeto Supabase.
-const SUPABASE_URL = 'https://noiisnlwdirxkepraejm.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_0K1RVdFPtwUrpSejT_WlPA_VZFLupkP';
+const VITE_SUPABASE_URL = 'https://noiisnlwdirxkepraejm.supabase.co';
+const VITE_SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_0K1RVdFPtwUrpSejT_WlPA_VZFLupkP';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
