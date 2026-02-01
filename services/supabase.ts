308f3810127caf5d441bb130fce570ef518cb881
Gabriel Pereira <arthur738pereira@gmail.com>
2026-02-01 16:55:17 -0300

    padronizando as chaves

--- services/supabase.ts
+++ services/supabase.ts
@@ -5,4 +5,4 @@ import { createClient } from '@supabase/supabase-js';
 const VITE_SUPABASE_URL = 'https://noiisnlwdirxkepraejm.supabase.co';
 const VITE_SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_0K1RVdFPtwUrpSejT_WlPA_VZFLupkP';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(VITE_SUPABASE_URL, VITE_SUPABASE_PUBLISHABLE_KEY);
